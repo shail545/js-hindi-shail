f5ef848d466578938e0216e07eed2ccd57e7703b
shail545 <141555992+shail545@users.noreply.github.com>
2024-11-24 11:07:44 +0000

    container config done

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 1444553
+let accountEmail =  "shailjma07@gmail.com"
+var accountPassword = "123459"
+accountCity = "Indore"   // esa ho skta h pr isko use nh krna h 
+let accountState  // isme agr kuch assign nh kiya h to undefined output aayega
+
+// let or var me changes kr skte h pr const me nh 
+
+accountEmail =" sgieb@gmail.com"
+accountPassword = "128"
+accountCity = "jaipur"
+
+/*
+prefer not to use var bcz of issue in block scope and functional scope
+*/
+
+console.table([accountEmail,accountId,accountPassword,accountCity,accountState]);  // console.table se bahut sari chije ek sath print krva skte h 
---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict";  // treat all js code as newer version
+ // alert(3+3) // we are using node js not browser
+
+let name = "Shail"
+let age  = 22
+let loggedIn = false
+
+// number => 2^53
+// bigint
+// string => ""
+// boolean => true , false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+console.log(typeof age)
+console.log(typeof null);  // object
+console.log(typeof undefined);  // undefined
+
+
