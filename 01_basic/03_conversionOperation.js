279a2c1b8e291bed011032d2d73eda0349d05625
shail545 <141555992+shail545@users.noreply.github.com>
2024-11-25 12:59:51 +0000

    container config done

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,47 @@
+// let score = 33
+// let score1 = "33"
+// let score2 = true
+// let score3 = null
+// let score4 = undefined
+// let score5 = "33ahbd"
+
+// let valueInNumber = Number(score)
+// let valueInNumber1 = Number(score1)
+// let valueInNumber2 = Number(score2)
+// let valueInNumber3 = Number(score3)
+// let valueInNumber4= Number(score4)
+// let valueInNumber5 = Number(score5)
+
+
+// console.log(typeof (score));   // number
+// console.log(typeof (score1));  // string
+// console.log(typeof (score2));  // boolean
+// console.log(typeof (score3));  // object
+// console.log(typeof (score4));  // undefined
+// console.log(typeof (score5));  // string
+
+// console.log( valueInNumber);  //33
+// console.log( valueInNumber1); //33
+// console.log( valueInNumber2); //1
+// console.log( valueInNumber3); //0
+// console.log( valueInNumber4); //NaN
+// console.log( valueInNumber5); //NaN
+
+
+// let isLoggedIn = ""   false
+// let isLoggedIn = 1  true
+// // let isLoggedIn = "shail"   ture
+// let booleanLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanLoggedIn);
+
+
+let someNumber = 33
+let numstr =String(someNumber)
+console.log(numstr);   // 33
+console.log(typeof numstr);   //string
+
+
+
+
+
+
